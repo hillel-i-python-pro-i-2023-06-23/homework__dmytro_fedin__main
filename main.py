e75e85b12da684ab19d2bcdb99d34727c9039b72
dafedin <tm.i.sm.master@gmail.com>
2023-06-28 07:11:33 +0300

    main method completed

--- main.py
+++ main.py
@@ -1,20 +1,74 @@
+is_cancelled = False
+words = []
+char = 'a'
 
+def get_word() -> str:
 
-def get_last_letter(string_):
-    return string_[len(string_)-1]
+    global words
 
-def get_first_letter(string_):
-    return string_[0]
+    def set_word():
+        word = input('Enter a word:')
+        return word
 
-def check_match(input_char, stored_char):
-    if input_char == stored_char:
+    word = set_word()
+
+    if type(word) == str:
+        return word
+    else:
+        print('not a word(')
+        return None
+
+def is_word(word) -> bool:
+
+    global words
+    global  char
+    def in_words(word_):
+        if word_ in words:
+            return True
+        else:
+            return False
+    def has_char(word):
+        if word[0] == char:
+            return True
+        else:
+            False
+
+    if not in_words(word) and has_char(word):
         return True
     else:
         return False
+        pass
+
+def is_cancelled(word):
+    if word == 'stop':
+        return True
+# Main method to run
+def main():
+    while True:
+
+        global  char
+
+        word = get_word()
+
+        print(words)
+        print(f'char: {char}')
+
+        if is_word(word):
+            print(f'good word: {word}')
+            words.append(word)
+            char = word[len(word) - 1]
+
+            print(f'char: {char}')
+            continue
+        elif not is_word(word):
+            print(f'bad word: {word}')
+            continue
+        elif is_cancelled(word):
+            break
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print(char2)
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
